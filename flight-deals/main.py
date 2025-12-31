e6f57267fa070a1db6940810596dc14d49e64e62
Damyr Ismailov <damyrismailov@gmail.com>
2025-12-31 19:34:29 +0300

    Create main.py

---
+++ flight-deals/main.py
@@ -0,0 +1,31 @@
+import time
+from datetime import datetime, timedelta
+from data_manager import DataManager
+from flight_search import FlightSearch
+from flight_data import find_cheapest_flight
+from notification_manager import NotificationManager
+data_manager = DataManager()
+sheets_data = data_manager.get_destination_data()
+flight_search = FlightSearch()
+notification_manager = NotificationManager()
+ORIGIN_CITY_IATA = "AYT"
+for row in sheets_data:
+    if row["iataCode"] == "":
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+        time.sleep(2)
+print(f"sheets_data: \n{sheets_data}")
+data_manager.destination_data = sheets_data
+data_manager.update_destination_codes()
+tomorrow = datetime.now() +timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6*30))
+for destination in sheets_data:
+    print(f"Getting flights for {destination['city']}...")
+    flights = flight_search.check_flights(ORIGIN_CITY_IATA,destination["iataCode"],from_time=tomorrow,to_time=six_month_from_today,)
+    cheapest_flight = find_cheapest_flight(flights)
+    if cheapest_flight.price != "N/A" and cheapest_flight.price < destination["lowestPrice"]:
+        print(f"Lower price flight found to {destination['city']}!")
+        notification_manager.send_sms(
+            message_body=f"Low price alert! Only 💶{cheapest_flight.price} to fly "
+                         f"from {cheapest_flight.origin_airport} to {cheapest_flight.destination_airport}, "
+                         f"on {cheapest_flight.out_date} until {cheapest_flight.return_date}."
+        )
